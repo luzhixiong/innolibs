e17b7cc035287d7f08013b021e80ed2f72f44823
luzhixiong <670404086@qq.com>
2025-02-07 17:40:27 +0800

    调整.

--- jpush/jpush.go
+++ jpush/jpush.go
@@ -7,10 +7,18 @@ import (
 
 const (
 	largeIcon = "https://static-us.innolabs.work/fly-logo.png"
+	smallIcon = "https://static-us.innolabs.work/fly-logo.png\""
 )
 
 // 极光推送推送给单个用户
-func JPushSingleNotify(jpushKey, jpushSecret string, platform PlatformType, id, title, content string) (err error) {
+func JPushSingleNotify(jpushKey, jpushSecret string, platform PlatformType, id, title, content string, largeUrl string, smallUrl string) (err error) {
+
+	if largeUrl == "" {
+		largeUrl = largeIcon
+	}
+	if smallUrl == "" {
+		smallUrl = smallIcon
+	}
 	cid := NewCidRequest(1, "")
 	cidList, err := cid.GetCidList(jpushKey, jpushSecret)
 	if err != nil {
@@ -31,7 +39,7 @@ func JPushSingleNotify(jpushKey, jpushSecret string, platform PlatformType, id, 
 		n.SetIos(&IosNotification{Alert: gin.H{"body": content, "title": title}, Badge: "+1", InterruptionLevel: "active", MutableContent: 1, Sound: "default", ThreadId: ""})
 	} else if platform == ANDROID {
 		n.Alert = content
-		n.SetAndroid(&AndroidNotification{Alert: content, Title: title, AlertType: 7, Intent: map[string]string{"url": ""}, Priority: 0, Category: "", BadgeAddNum: 1, LargeIcon: largeIcon})
+		n.SetAndroid(&AndroidNotification{Alert: content, Title: title, AlertType: 7, Intent: map[string]string{"url": ""}, Priority: 0, Category: "", BadgeAddNum: 1, LargeIcon: largeUrl, SmallIconUri: smallUrl})
 	}
 	// 构建消息
 	//var m jpush.Message
